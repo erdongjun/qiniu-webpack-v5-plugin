d5fe14a962fecbdca781225bac46deaf1babfd70
chenweizhi <15647062+erdongjun@users.noreply.github.com>
2023-11-09 11:43:41 +0800

    fix: 兼容v5版本

--- src/QiniuPlugin.js
+++ src/QiniuPlugin.js
@@ -3,8 +3,7 @@ import Promise from 'promise';
 import { join } from 'path';
 import slash from 'slash';
 
-class QiniuPlugin {
-
+class QiNiuPlugin {
   constructor(options) {
     if (!options || !options.ACCESS_KEY || !options.SECRET_KEY) {
       throw new Error('ACCESS_KEY and SECRET_KEY must be provided');
@@ -15,67 +14,77 @@ class QiniuPlugin {
   }
 
   apply(compiler) {
-    compiler.plugin('after-emit', (compilation, callback) => {
-      const assets = compilation.assets;
-      const hash = compilation.hash;
-      const {
-        bucket,
-        include,
-      } = this.options;
-      let {
-        path = '[hash]',
-      } = this.options;
+    let func = null;
+    if (compiler.hooks) {
+      func = compiler.hooks.afterEmit.tapAsync.bind(
+        compiler.hooks.afterEmit,
+        'QiNiuPlugin '
+      );
+    } else {
+      func = compiler.plugin.bind(compiler, 'afterEmit');
+    }
+    func(
+      async (compilation, callback) => {
+        const assets = compilation.assets;
+        const hash = compilation.hash;
+        const {
+          bucket,
+          include,
+        } = this.options;
+        let {
+          path = '[hash]',
+        } = this.options;
 
-      path = path.replace('[hash]', hash);
+        path = path.replace('[hash]', hash);
 
-      const promises = Object.keys(assets).filter((fileName) => {
-        let valid = assets[fileName].emitted;
-        if (include) {
-          valid = valid
-            && include.some(
-              (includeFileName) => {
-                if (includeFileName instanceof RegExp) {
-                  return includeFileName.test(fileName);
-                }
-                return includeFileName === fileName;
-              },
-            );
-        }
-        return valid;
-      }).map((fileName) => {
-        const key = slash(join(path, fileName));
-        const putPolicy = new qiniu.rs.PutPolicy(`${bucket}:${key}`);
-        const token = putPolicy.token();
-        const extra = new qiniu.io.PutExtra();
+        const promises = Object.keys(assets).filter((fileName) => {
+          let valid = assets[fileName].emitted;
+          if (include) {
+            valid = valid
+                && include.some(
+                  (includeFileName) => {
+                    if (includeFileName instanceof RegExp) {
+                      return includeFileName.test(fileName);
+                    }
+                    return includeFileName === fileName;
+                  },
+                );
+          }
+          return valid;
+        }).map((fileName) => {
+          const key = slash(join(path, fileName));
+          const putPolicy = new qiniu.rs.PutPolicy(`${bucket}:${key}`);
+          const token = putPolicy.token();
+          const extra = new qiniu.io.PutExtra();
 
-        const promise = new Promise((resolve, reject) => {
-          const begin = Date.now();
-          qiniu.io.putFile(token, key, assets[fileName].existsAt, extra, (err, ret) => {
-            if (!err) {
-              resolve({
-                ...ret,
-                duration: Date.now() - begin,
-              });
-            } else {
-              reject(err);
-            }
+          const promise = new Promise((resolve, reject) => {
+            const begin = Date.now();
+            qiniu.io.putFile(token, key, assets[fileName].existsAt, extra, (err, ret) => {
+              if (!err) {
+                resolve({
+                  ret,
+                  duration: Date.now() - begin,
+                });
+              } else {
+                reject(err);
+              }
+            });
           });
+
+          return promise;
         });
 
-        return promise;
-      });
-
-      Promise
-        .all(promises)
-        .then((res) => {
-          console.log(res); // eslint-disable-line no-console
-          callback();
-        })
-        .catch((e) => {
-          callback(e);
-        });
-    });
+        Promise
+          .all(promises)
+          .then((res) => {
+            console.log(res); // eslint-disable-line no-console
+            callback();
+          })
+          .catch((e) => {
+            callback(e);
+          });
+      }
+    );
   }
 }
-
-export default QiniuPlugin;
+export default QiNiuPlugin;
